3246aabeb51502ffaaf4c0207cdc58c311354c70
Kasramoradi86 <KasrafreelancerENG86@gmail.com>
2026-07-09 17:04:19 +0430

    Setup database and SQLAlchemy models

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from .database import engine
+from . import models
 
 app = FastAPI(title="Student Management API",description="A simple REST API for student management built with FASTAPI",version="1.0.0")
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
---
+++ app/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column,String,Integer,ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Student(Base):
+    __tablename__ = "students"
+    id = Column(Integer,primary_key=True)
+    name = Column(String,nullable=False)
+    age = Column(Integer,nullable=False)
+    email = Column(String,nullable=True,unique=True)
+    classroom_id = Column(Integer,ForeignKey("classrooms.id"))
+    
+    student_class = relationship("Classroom",back_populates="students")
+
+class ClassRoom(Base):
+    __tablename__ = "classrooms"
+    id = Column(Integer,primary_key=True)
+    name = Column(String,nullable=False)
+
+    students = relationship("Student",back_populates="student_class")
